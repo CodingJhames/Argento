119b5065f2301b6f10a4de55612267fd14a2aef8
codingjhames <jhamesmejiadev@gmail.com>
2023-12-18 17:50:23 -0500

    properties section finished

--- main/poo.py
+++ main/poo.py
@@ -42,6 +42,28 @@ print(example_object_1.__dict__, example_object_1.counter)
 print(example_object_2.__dict__, example_object_2.counter)
 print(example_object_3.__dict__, example_object_3.counter)
 
+class ExampleThree:
+    varia = 1
+    def __init__(self, val):
+        ExampleThree.varia = val
 
 
-   
+print(ExampleThree.__dict__)
+example_object = ExampleThree(2)
+
+print(ExampleThree.__dict__)
+print(example_object.__dict__)
+    
+class ExampleClassFour:
+    def __init__(self, val):
+        if val % 2 != 0:
+            self.a = 1
+        else:
+            self.b = 1
+
+
+example_object = ExampleClassFour(1)
+print(example_object.a)
+
+if hasattr(example_object, 'b'):
+    print(example_object.b)
